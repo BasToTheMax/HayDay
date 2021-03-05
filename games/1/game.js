c31ed2269a4e1b86ca460a3ae4607cdf6fe07286
BasToTheMax <55401336+BasToTheMax@users.noreply.github.com>
2021-03-05 12:18:01 +0100

    Update game.js

--- games/1/game.js
+++ games/1/game.js
@@ -7,15 +7,10 @@ const close = document.getElementById('modal-close');
 if (localStorage.getItem("zndVh91F4Z/ID") == null || localStorage.getItem("zndVh91F4Z/ID") == undefined || localStorage.getItem("zndVh91F4Z/ID") == '') {
     localStorage.setItem("zndVh91F4Z/ID", Math.floor(Math.random() * 100000));
 } else {
-    $(document).ready(function(){
-        /* $.get("http://volatile.wtf/?key=zndVh91F4Z/hayday/level/", function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
-        }); */
-    });
 
-    fetch('https://hayday-8a4d.restdb.io/rest/users/')
+    /*fetch('https://hayday-8a4d.restdb.io/rest/users/')
     .then(res => res.json())
-    .then(json => console.log(json));
+    .then(json => console.log(json)); */
 }
 
 if (localStorage.getItem('boer') == '' || localStorage.getItem('boer') == null) {
@@ -27,5 +22,23 @@ function boerMenu() {
   close.style.display='none';
     
   title.innerHTML = 'Welkom!';
-    content.innerHTML = '<h1>Welkom!</h1><h2>Wat is de naam van je boerderij?</h2><input id="input_naam" placeholder="Naam van je boerderij"><hr><h2>Wat ben je?</h2><p id="boer_tekst">Kies:<br></p><button id="button_boer" class="w3-button w3-purple w3-round-xlarge">Boer</button><hr>';
+    content.innerHTML = '<h1>Welkom!</h1><h2>Wat is de naam van je boerderij?</h2><input id="input_naam" class="w3-input w3-border" placeholder="Naam van je boerderij"><hr><h2>Wat ben je?</h2><p id="boer_tekst">Kies:<br></p><button onlcick="kies_boer()" class="w3-button w3-purple w3-round-xlarge">Boer</button> <button onclick="kies_boerin()" class="w3-button w3-purple w3-round-xlarge">Boerin</button><hr><button onclick="createAccount()">Maak account!</button>';
+}
+function createAccount() {
+    var naam = document.getElementById('input_naam');
+    
+    localStorage.setItem('naam', naam.value);
+}
+
+function kies_boer() {
+    var tekst = document.getElementById('boer_tekst');
+    
+    tekst.innerHTML = 'Boer!';
+    localStorage.setItem("boer", "boer")
+}
+function kies_boerin() {
+    var tekst = document.getElementById('boer_tekst');
+    
+    tekst.innerHTML = 'Boerin!';
+    localStorage.setItem("boer", "boerin")
 }
