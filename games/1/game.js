a14fdab963fb00b83d9a09a9bfff35f29eef5c40
BasToTheMax <55401336+BasToTheMax@users.noreply.github.com>
2021-03-05 10:25:07 +0100

    Update game.js

--- games/1/game.js
+++ games/1/game.js
@@ -1,6 +1,8 @@
 const title = document.getElementById('modal-title');
 const content = document.getElementById('modal-content');
 const footer = document.getElementById('modal-footer');
+const close = document.getElementById('modal-close');
+
 
 if (localStorage.getItem("zndVh91F4Z/ID") == null || localStorage.getItem("zndVh91F4Z/ID") == undefined || localStorage.getItem("zndVh91F4Z/ID") == '') {
     localStorage.setItem("zndVh91F4Z/ID", Math.floor(Math.random() * 100000));
@@ -22,4 +24,8 @@ if (localStorage.getItem('boer') == '' || localStorage.getItem('boer') == null) 
 
 function boerMenu() {
   document.getElementById('modal').style.display='block'
+  close.style.display='none';
+    
+  title.innerHTML = 'Welkom!';
+    content = '<h1>Welkom!</h1><h2>Wat is de naam van je boerderij?</h2><input id="input_naam" placeholder="Naam van je boerderij"><hr><h2>Wat ben je?</h2><p id="boer_tekst">Kies:<br></p><button id="button_boer" class="w3-button w3-purple w3-round-xlarge">Boer</button><hr>';
 }
